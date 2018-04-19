4b2e0424277ea830ac6f21d7ff53b0e524b38fc7
Shane McDonald <me@shanemcd.com>
2018-04-19 10:58:28 -0400

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ansible-runner",
-    version="1.0",
+    version="1.0.0",
     author='Red Hat Ansible',
     url="https://github.com/ansible/ansible-runner",
     packages=find_packages(),

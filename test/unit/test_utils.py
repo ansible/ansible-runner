b21adbd65e995ed2358b463cdd306e088cb3dd9b
Peter Sprygada <psprygad@redhat.com>
2018-04-24 17:11:22 -0400

    adds unit test cases for interface changes
    This adds unit test cases for the changes merged in #21.  This change
    also adds some minor bug fixes exposed by the unit test cases.
    
    The unit test cases can be run using pytest from the project root
    folder.

---
+++ test/unit/test_utils.py
@@ -0,0 +1,159 @@
+import os
+import json
+import shutil
+
+from pytest import raises
+from mock import patch
+
+import ansible_runner.utils
+
+from ansible_runner.utils import isplaybook, isinventory
+from ansible_runner.utils import to_artifacts, dump_artifact
+
+
+def test_isplaybook():
+
+    for obj in ('foo', {}, {'foo': 'bar'}, True, False, None):
+        assert isplaybook(obj) is False, obj
+
+    for obj in (['foo'], []):
+        assert isplaybook(obj) is True, obj
+
+def test_isinventory():
+    for obj in (__file__, {}, {'foo': 'bar'}):
+        assert isinventory(obj) is True, obj
+
+    for obj in ([], ['foo'], True, False, None):
+        assert isinventory(obj) is False, obj
+
+def test_to_artifacts_private_data_dir():
+    data_dir = '/tmp'
+    kwargs = {'private_data_dir': data_dir}
+    to_artifacts(kwargs)
+    assert kwargs['private_data_dir'] == data_dir
+
+    kwargs = {'private_data_dir': None}
+    to_artifacts(kwargs)
+    assert kwargs['private_data_dir'].startswith('/tmp/tmp')
+    shutil.rmtree(kwargs['private_data_dir'])
+
+    with raises(ValueError):
+        data_dir = '/foo'
+        kwargs = {'private_data_dir': data_dir}
+        to_artifacts(kwargs)
+
+
+def test_to_artifacts_playbook():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        # playbook as a native object
+        pb = [{'playbook': [{'hosts': 'all'}]}]
+        kwargs = {'private_data_dir': '/tmp', 'playbook': pb}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == json.dumps(pb)
+        assert fp == '/tmp/project'
+        assert fn == 'main.json'
+
+        mock_dump_artifact.reset_mock()
+
+        # playbook as a path
+        pb = 'test.yml'
+        kwargs = {'private_data_dir': '/tmp', 'playbook': pb}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 0
+        assert mock_dump_artifact.called is False
+
+        mock_dump_artifact.reset_mock()
+
+        # invalid playbook structures
+        for obj in ({'foo': 'bar'}, None, True, False, 'foo', []):
+            mock_dump_artifact.reset_mock()
+            kwargs = {'private_data_dir': '/tmp', 'playbook': obj}
+            to_artifacts(kwargs)
+            assert mock_dump_artifact.call_count == 0
+            assert mock_dump_artifact.called is False
+
+
+def test_to_artifacts_inventory():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        # inventory as a string (INI)
+        inv = '[all]\nlocalhost'
+        kwargs = {'private_data_dir': '/tmp', 'inventory': inv}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == inv
+        assert fp == '/tmp/inventory'
+        assert fn == 'hosts'
+
+        mock_dump_artifact.reset_mock()
+
+        # inventory as a path
+        inv = '/tmp'
+        kwargs = {'private_data_dir': '/tmp', 'inventory': inv}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 0
+        assert mock_dump_artifact.called is False
+        assert kwargs['inventory'] == inv
+
+        mock_dump_artifact.reset_mock()
+
+        # inventory as a native object
+        inv = {'foo': 'bar'}
+        kwargs = {'private_data_dir': '/tmp', 'inventory': inv}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == json.dumps(inv)
+        assert fp == '/tmp/inventory'
+        assert fn == 'hosts.json'
+
+
+def test_to_artifacts_extravars():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        extravars = {'foo': 'bar'}
+        kwargs = {'private_data_dir': '/tmp', 'extravars': extravars}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == json.dumps(extravars)
+        assert fp == '/tmp/env'
+        assert fn == 'extravars'
+        assert 'extravars' not in kwargs
+
+def test_to_artifacts_passwords():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        passwords = {'foo': 'bar'}
+        kwargs = {'private_data_dir': '/tmp', 'passwords': passwords}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == json.dumps(passwords)
+        assert fp == '/tmp/env'
+        assert fn == 'passwords'
+        assert 'passwords' not in kwargs
+
+def test_to_artifacts_settings():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        settings = {'foo': 'bar'}
+        kwargs = {'private_data_dir': '/tmp', 'settings': settings}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == json.dumps(settings)
+        assert fp == '/tmp/env'
+        assert fn == 'settings'
+        assert 'settings' not in kwargs
+
+def test_to_artifacts_ssh_key():
+    with patch('ansible_runner.utils.dump_artifact') as mock_dump_artifact:
+        ssh_key = '1234567890'
+        kwargs = {'private_data_dir': '/tmp', 'ssh_key': ssh_key}
+        to_artifacts(kwargs)
+        assert mock_dump_artifact.call_count == 1
+        data, fp, fn = mock_dump_artifact.call_args[0]
+        assert data == ssh_key
+        assert fp == '/tmp/env'
+        assert fn == 'ssh_key'
+        assert 'ssh_key' not in kwargs

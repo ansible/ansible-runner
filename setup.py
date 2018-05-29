f4cdf9400736f2e40888d7a2a25414c1958c12c7
Matthew Jones <matburt@redhat.com>
2018-05-29 16:06:33 -0400

    Bumping release for 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ansible-runner",
-    version="1.0.1",
+    version="1.0.2",
     author='Red Hat Ansible',
     url="https://github.com/ansible/ansible-runner",
     packages=find_packages(),

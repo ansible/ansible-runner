a66ec0f41304e05647156985f243306bf828ea46
Daniel-Pivonka <dpivonka@dhcp-41-124.bos.redhat.com>
2018-06-28 10:38:23 -0400

    Revert "merge changes"
    This reverts commit 3c2130f122922283190126c006a8f93682f9e6dd, reversing
    changes made to c4d2df927fb2b9185771b4261e7aa52ffa9c1e93.

--- test/unit/test_runner_config.py
+++ test/unit/test_runner_config.py
@@ -186,13 +186,8 @@ def test_generate_ansible_command():
     rc.extra_vars = '/env/extravars'
     cmd = rc.generate_ansible_command()
     assert cmd == ['ansible-playbook', '-i', '/inventory', '-e', '@/env/extravars', 'main.yaml']
+
     rc.extra_vars = None
-
-    rc.verbosity = 3
-    cmd = rc.generate_ansible_command()
-    assert cmd == ['ansible-playbook', '-i', '/inventory', '-vvv', 'main.yaml']
-    rc.verbosity = None
-
     rc.limit = 'hosts'
     cmd = rc.generate_ansible_command()
     assert cmd == ['ansible-playbook', '-i', '/inventory', '--limit', 'hosts', 'main.yaml']
@@ -204,7 +199,6 @@ def test_generate_ansible_command():
     rc.module_args = 'test=string'
     cmd = rc.generate_ansible_command()
     assert cmd == ['ansible', '-i', '/inventory', '--limit', 'hosts', '-m', 'setup', '-a', 'test=string']
-
 
 
 def test_prepare_command_defaults():

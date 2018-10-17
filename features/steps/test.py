b0038fd80fd201a323a296855a9c6f990a7e5d32
Ben Thomasson <bthomass@redhat.com>
2018-10-17 16:43:16 -0400

    Starting bdd branch

---
+++ features/steps/test.py
@@ -0,0 +1,12 @@
+
+from behave import given
+
+
+@given(u'some condition')
+def step_impl(context):
+    pass
+
+
+@then(u'some result is expected')
+def step_impl(context):
+    pass
---
+++ features/test.feature
@@ -0,0 +1,5 @@
+Feature: feature name
+
+    Scenario: some scenario
+        Given some condition
+        Then some result is expected

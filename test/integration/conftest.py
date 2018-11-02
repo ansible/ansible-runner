11f59dc863af818275b6d213c56da06807ccb2b1
Matthew Jones <matburt@redhat.com>
2018-11-02 10:33:22 -0400

    Fix up runner_on_start and add some test coverage for events

---
+++ test/integration/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+import pexpect
+from ansible_runner.runner_config import RunnerConfig
+
+
+@pytest.fixture(scope='function')
+def rc(request, tmpdir):
+    rc = RunnerConfig(str(tmpdir))
+    rc.suppress_ansible_output = True
+    rc.expect_passwords = {
+        pexpect.TIMEOUT: None,
+        pexpect.EOF: None
+    }
+    rc.cwd = str(tmpdir)
+    rc.env = {}
+    rc.job_timeout = .1
+    rc.idle_timeout = 0
+    rc.pexpect_timeout = .1
+    rc.pexpect_use_poll = True
+    return rc

b86d103dbe26f03821eb24e955f8c9f6a6c006fd
Jim Ladd <jladd@redhat.com>
2019-02-27 00:21:31 -0800

    Add new playbook stats fields (ignored, rescued)

--- test/integration/test_events.py
+++ test/integration/test_events.py
@@ -46,3 +46,16 @@ def test_runner_on_start(rc):
     start_events = [x for x in filter(lambda x: 'event' in x and x['event'] == 'runner_on_start',
                                       r.events)]
     assert len(start_events) == 1
+
+def test_playbook_on_stats_summary_fields(rc):
+    tdir = tempfile.mkdtemp()
+    r = run(private_data_dir=tdir,
+            inventory="localhost ansible_connection=local",
+            playbook=[{'hosts': 'all', 'gather_facts': False, 'tasks': [{'debug': {'msg': "test"}}]}])
+    stats_events = [x for x in filter(lambda x: 'event' in x and x['event'] == 'playbook_on_stats',
+                                      r.events)]
+    assert len(stats_events) == 1
+
+    EXPECTED_SUMMARY_FIELDS = ('changed', 'dark', 'failures', 'ignored', 'ok', 'rescued', 'skipped')
+    fields = stats_events[0]['event_data'].keys()
+    assert set(fields) >= set(EXPECTED_SUMMARY_FIELDS)

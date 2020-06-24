fee5bc67953f72c2b556633674c86047c4e2072d
Elijah DeLee <kdelee@redhat.com>
2020-06-24 18:37:42 -0400

    devel is future 2.x
    1.x is now living on a stable branch

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Red Hat Ansible'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.4.6'
+release = '2.0.0'
 
 
 # -- General configuration ---------------------------------------------------

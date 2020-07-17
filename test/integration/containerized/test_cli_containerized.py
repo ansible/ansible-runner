278bd4509ab1cd25eab214ae5ed63473c0148af1
Jim Ladd <jladd@redhat.com>
2020-07-17 13:41:19 -0700

    run containerized cli tests serially

--- test/integration/containerized/test_cli_containerized.py
+++ test/integration/containerized/test_cli_containerized.py
@@ -6,12 +6,14 @@ import pytest
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
+@pytest.mark.serial
 def test_module_run(cli, container_runtime_available):
     if not container_runtime_available:
         pytest.skip('container runtime(s) not available')
     cli(['-m', 'ping','--hosts', 'localhost', 'run', os.path.join(HERE, 'priv_data')])
 
 
+@pytest.mark.serial
 def test_playbook_run(cli, container_runtime_available):
     if not container_runtime_available:
         pytest.skip('container runtime(s) not available')

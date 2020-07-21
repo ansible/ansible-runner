202cf8b89146ba6f2cbb7f306e4ab3ead827d932
Shane McDonald <shanemcd@redhat.com>
2020-07-21 15:21:48 -0400

    Prevent confirmation prompt when removing /tmp/passwd
    I'm not sure why we were writing to /tmp/passwd first. This seems to work.

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -5,14 +5,10 @@
 # require a named user. So if we're in OpenShift, we need to make
 # one before Ansible runs.
 if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
-
-  cat << EOF > /tmp/passwd
+cat << EOF > /etc/passwd
 root:x:0:0:root:/root:/bin/bash
 runner:x:`id -u`:`id -g`:,,,:/runner:/bin/bash
 EOF
-
-  cat /tmp/passwd > /etc/passwd
-  rm /tmp/passwd
 fi
 
 exec tini -- "${@}"

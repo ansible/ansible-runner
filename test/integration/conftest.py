d09acc825d46f6c95d3801bd209678ff810cd18a
Adam Miller <admiller@redhat.com>
2020-08-04 17:26:22 -0500

    add basic integraation tests for cli execenv
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -35,3 +35,19 @@ def container_runtime_available():
             warnings.warn(UserWarning(f"{runtime} not available"))
             runtimes_available = False
     return runtimes_available
+
+
+# TODO: determine if we want to add docker / podman
+# to zuul instances in order to run these tests
+@pytest.fixture(scope="session", autouse=True)
+def container_runtime_installed(container_runtime_available):
+    import subprocess
+    import warnings
+
+    if container_runtime_available:
+        for runtime in ('podman', 'docker'):
+            try:
+                subprocess.run([runtime, '-v'])
+                return runtime
+            except FileNotFoundError:
+                pass

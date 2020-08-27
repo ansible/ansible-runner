1df67b9c96df365637eb0c9450f81363d27d0791
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-08-27 11:31:08 -0400

    Deal with bytes vs str

--- ansible_runner/streaming.py
+++ ansible_runner/streaming.py
@@ -8,15 +8,20 @@ class StreamWorker(object):
     def __init__(self, control_out):
         self.control_out = control_out
 
-    def status_handler(self, status):
-        json.dump(status, self.control_out)
+    def status_handler(self, status, runner_config):
+        self.control_out.write(json.dumps(status).encode('utf-8'))
+        self.control_out.write(b'\n')
         self.control_out.flush()
 
     def event_handler(self, event_data):
-        json.dump(event_data, self.control_out)
+        self.control_out.write(json.dumps(event_data).encode('utf-8'))
+        self.control_out.write(b'\n')
         self.control_out.flush()
 
     def artifacts_callback(self, artifact_dir):
+        self.control_out.write(json.dumps({'artifacts': True}).encode('utf-8'))
+        self.control_out.write(b'\n')
+
         buf = io.BytesIO()
         with zipfile.ZipFile(buf, 'w', compression=zipfile.ZIP_DEFLATED, allowZip64=True) as archive:
             for dirpath, dirs, files in os.walk(artifact_dir):

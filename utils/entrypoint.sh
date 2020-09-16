f51ec11d32d190fee352244965028d308d1504dc
Shane McDonald <me@shanemcd.com>
2020-09-16 18:17:10 -0400

    Fix permissions error when host uid doesnt match container user id

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -4,11 +4,24 @@
 # that doesn't exist in /etc/passwd, but Ansible module utils
 # require a named user. So if we're in OpenShift, we need to make
 # one before Ansible runs.
-if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
+if [[ (`id -u` -ge 500 || -z "${CURRENT_UID}") ]]; then
+
+    # Only needed for RHEL 8. Try deleting this conditional (not the code)
+    # sometime in the future. Seems to be fixed on Fedora 32
+    # If we are running in rootless podman, this file cannot be overwritten
+    ROOTLESS_MODE=$(cat /proc/self/uid_map | head -n1 | awk '{ print $2; }')
+    if [[ "$ROOTLESS_MODE" -eq "0" ]]; then
 cat << EOF > /etc/passwd
 root:x:0:0:root:/root:/bin/bash
 runner:x:`id -u`:`id -g`:,,,:/home/runner:/bin/bash
 EOF
+    fi
+
+cat <<EOF > /etc/group
+root:x:0:runner
+runner:x:`id -g`:
+EOF
+
 fi
 
 if [[ -n "${LAUNCHED_BY_RUNNER}" ]]; then

6d4d1584503c075ca425a9b15729ebc7547ef3a3
Ganesh B. Nalawade <ganesh634@gmail.com>
2021-05-11 11:29:16 -0400

    fix unit test failures

--- test/unit/config/test_inventory.py
+++ test/unit/config/test_inventory.py
@@ -105,8 +105,8 @@ def test_prepare_inventory_command_with_containerization(tmpdir, container_runti
     if container_runtime == 'podman':
         expected_command_start +=['--group-add=root', '--userns=keep-id', '--ipc=host']
 
-    expected_command_start += ['-v', '{}/artifacts:/runner/artifacts:Z'.format(rc.private_data_dir)] + \
-        ['-v', '{}:/runner:Z'.format(rc.private_data_dir)] + \
+    expected_command_start += ['-v', '{}/artifacts/:/runner/artifacts:Z'.format(rc.private_data_dir)] + \
+        ['-v', '{}/:/runner:Z'.format(rc.private_data_dir)] + \
         ['--env-file', '{}/env.list'.format(rc.artifact_dir)] + \
         extra_container_args + \
         ['--name', 'ansible_runner_foo', 'my_container'] + \

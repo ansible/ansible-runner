3e1e2bf72b236c3b5df193df98aaec27fd582699
Sarabraj Singh <sarsingh@redhat.com>
2021-05-13 12:40:49 -0400

    retired try/except block in favour of a if/else conditional that raises an Exception when an invalid symlink is surfaced.
    removed un-used import
    
    fixing 'ansible_runner/utils/streaming.py:10:1: E302 expected 2 blank lines, found 1' thrown by flake8
    
    formatted ansible_runner/utils/streaming.py file with python-black

--- ansible_runner/utils/streaming.py
+++ ansible_runner/utils/streaming.py
@@ -10,34 +10,39 @@ from pathlib import Path
 
 def stream_dir(source_directory, stream):
     with tempfile.NamedTemporaryFile() as tmp:
-        with zipfile.ZipFile(tmp.name, 'w', compression=zipfile.ZIP_DEFLATED, allowZip64=True) as archive:
+        with zipfile.ZipFile(
+            tmp.name, "w", compression=zipfile.ZIP_DEFLATED, allowZip64=True
+        ) as archive:
             if source_directory:
                 for dirpath, dirs, files in os.walk(source_directory):
                     relpath = os.path.relpath(dirpath, source_directory)
                     if relpath == ".":
                         relpath = ""
                     for fname in files:
-                        try:
-                            archive.write(os.path.join(dirpath, fname), arcname=os.path.join(relpath, fname))
-                        except FileNotFoundError as e:
-                            e.strerror = "invalid symlink detected"
-                            raise e
+                        fullpath = os.path.join(dirpath, fname)
+                        if not os.path.isfile(fullpath):
+                            raise FileNotFoundError(
+                                "invalid symlink found; file={0}.".format(fullpath)
+                            )
+                        archive.write(
+                            os.path.join(dirpath, fname), arcname=os.path.join(relpath, fname)
+                        )
             archive.close()
 
         zip_size = Path(tmp.name).stat().st_size
 
         with open(tmp.name, "rb") as source:
-            if stream.name == '<stdout>':
+            if stream.name == "<stdout>":
                 target = sys.stdout.buffer
-            else: 
+            else:
                 target = stream
-            target.write(json.dumps({'zipfile': zip_size}).encode('utf-8') + b'\n')
+            target.write(json.dumps({"zipfile": zip_size}).encode("utf-8") + b"\n")
             with Base64IO(target) as encoded_target:
                 for line in source:
                     encoded_target.write(line)
 
 
-def unstream_dir(stream, length, target_directory):            
+def unstream_dir(stream, length, target_directory):
     # NOTE: caller needs to process exceptions
     with tempfile.NamedTemporaryFile() as tmp:
         with open(tmp.name, "wb") as target:
@@ -53,7 +58,7 @@ def unstream_dir(stream, length, target_directory):
 
                     remaining -= chunk_size
 
-        with zipfile.ZipFile(tmp.name, 'r') as archive:
+        with zipfile.ZipFile(tmp.name, "r") as archive:
             # Fancy extraction in order to preserve permissions
             # https://www.burgundywall.com/post/preserving-file-perms-with-python-zipfile-module
             for info in archive.infolist():

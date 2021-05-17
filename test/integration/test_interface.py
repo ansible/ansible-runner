99394ae0641cd07b3fb84077a093f1505b104542
Ganesh B. Nalawade <ganesh634@gmail.com>
2021-05-17 18:28:26 -0400

    Return error code for run_command interface
    *  Update the return value to run_command
       interface API to return the return code
       from the process that excecutes the command
    
    (cherry picked from commit eb2731a775240684d6c4ee53c44103cccbe479d8)

--- test/integration/test_interface.py
+++ test/integration/test_interface.py
@@ -142,12 +142,13 @@ def test_run_command(test_data_dir):
     private_data_dir = os.path.join(test_data_dir, 'debug')
     inventory = os.path.join(private_data_dir, 'inventory', 'inv_1')
     playbook = os.path.join(private_data_dir, 'project', 'debug.yml')
-    out, err = run_command(
+    rc, out, err = run_command(
         private_data_dir=private_data_dir,
         executable_cmd='ansible-playbook',
         cmdline_args=[playbook, '-i', inventory]
     )
     assert "Hello world!" in out
+    assert rc == 0
     assert err == ''
 
 
@@ -160,13 +161,14 @@ def test_run_ansible_command_within_container(test_data_dir, container_runtime_i
         'process_isolation': True,
         'container_image': defaults.default_container_image
     }
-    out, err = run_command(
+    rc, out, err = run_command(
         private_data_dir=private_data_dir,
         executable_cmd='ansible-playbook',
         cmdline_args=[playbook, '-i', inventory],
         **container_kwargs
     )
     assert "Hello world!" in out
+    assert rc == 0
     assert err == ''
 
 
@@ -180,7 +182,7 @@ def test_run_script_within_container(test_data_dir, container_runtime_installed)
         'container_image': defaults.default_container_image,
         'container_volume_mounts': container_volume_mounts
     }
-    out, _ = run_command(
+    rc, out, _ = run_command(
         private_data_dir=private_data_dir,
         executable_cmd='python3',
         cmdline_args=[os.path.join(script_path, 'test_ee.py')],
@@ -188,6 +190,7 @@ def test_run_script_within_container(test_data_dir, container_runtime_installed)
     )
 
     assert "os-release" in out
+    assert rc == 0
 
 
 def test_run_command_async(test_data_dir):

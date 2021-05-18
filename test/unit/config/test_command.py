ed1338c0c503c7a1560cf3205a22e874eb496cda
Ganesh B. Nalawade <ganesh634@gmail.com>
2021-05-18 04:19:45 -0400

    Update runner config to use common code
    *  Move common code from `config/runner.py` (RunnerConfig)
       to `config/_base.py` (BaseConfig)
    
    *  Update testcases
    
    (cherry picked from commit adbd5a835ebaee41bf4e33614619be1a4db2649e)

--- test/unit/config/test_command.py
+++ test/unit/config/test_command.py
@@ -79,6 +79,7 @@ def test_prepare_run_command_with_containerization(tmpdir, container_runtime):
 
     expected_command_start = [container_runtime, 'run', '--rm', '--interactive', '--tty', '--workdir', '/runner/project'] + \
                              ['-v', '{}/:{}'.format(cwd, cwd), '-v', '{}/.ssh/:/home/runner/.ssh/'.format(os.environ['HOME'])]
+
     if container_runtime == 'podman':
         expected_command_start +=['--group-add=root', '--userns=keep-id', '--ipc=host']
 

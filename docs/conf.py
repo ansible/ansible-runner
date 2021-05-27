def0873bcc8e2d4403bfbd98f21e9819907067dd
Shane McDonald <me@shanemcd.com>
2021-05-27 09:55:34 -0400

    Bump version for 2.0 alpha 4

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Red Hat Ansible'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.0.0a3'
+release = '2.0.0a4'
 
 
 # -- General configuration ---------------------------------------------------

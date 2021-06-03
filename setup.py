c52cf7dcb0183e51d7521880b30d65496b1e48da
Shane McDonald <me@shanemcd.com>
2021-06-03 19:13:23 -0400

    Drop Poetry in favor of PBR

--- setup.py
+++ setup.py
@@ -1,32 +1,8 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2018 Red Hat, Inc.
-# All Rights Reserved.
-
-from setuptools import setup, find_packages
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
+from setuptools import setup
 
 setup(
-    name="ansible-runner",
-    version="2.0.0a4",
-    author='Red Hat Ansible',
-    url="https://github.com/ansible/ansible-runner",
-    license='Apache',
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'pexpect>=4.5',
-        'python-daemon',
-        'PyYAML',
-        'six',
-    ],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'ansible-runner = ansible_runner.__main__:main'
-        ]
-    }
+    setup_requires=['pbr'],
+    pbr=True,
 )

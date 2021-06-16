83ce4f6a0b5ac6e3b3c2d8abef7bc49e8c20a541
cidrblock <brad@thethorntons.net>
2021-06-16 06:31:18 -0700

    Fix, test
    (cherry picked from commit ec5a831152229ade0669e22d50b685519bfa1407)

--- test/unit/config/test__base.py
+++ test/unit/config/test__base.py
@@ -261,7 +261,7 @@ def test_container_volume_mounting_with_Z(mock_isdir, mock_exists, mock_makedirs
     for i, entry in enumerate(new_args):
         if entry == '-v':
             mount = new_args[i + 1]
-            if mount.endswith('project_path:Z'):
+            if mount.endswith('project_path/:Z'):
                 break
     else:
         raise Exception('Could not find expected mount, args: {}'.format(new_args))
@@ -295,8 +295,8 @@ def test_containerization_settings(tmpdir, container_runtime):
     if container_runtime == 'podman':
         expected_command_start +=['--group-add=root', '--userns=keep-id', '--ipc=host']
 
-    expected_command_start += ['-v', '{}/artifacts/:/runner/artifacts:Z'.format(rc.private_data_dir)] + \
-        ['-v', '{}/:/runner:Z'.format(rc.private_data_dir)] + \
+    expected_command_start += ['-v', '{}/artifacts/:/runner/artifacts/:Z'.format(rc.private_data_dir)] + \
+        ['-v', '{}/:/runner/:Z'.format(rc.private_data_dir)] + \
         ['--env-file', '{}/env.list'.format(rc.artifact_dir)] + \
         extra_container_args + \
         ['--name', 'ansible_runner_foo'] + \

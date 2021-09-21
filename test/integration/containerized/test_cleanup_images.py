ecaaca1ea245e980114d9f18f3f93358f93bc35e
Alan Rominger <arominge@redhat.com>
2021-09-21 13:47:34 -0400

    Get integration test running

--- test/integration/containerized/test_cleanup_images.py
+++ test/integration/containerized/test_cleanup_images.py
@@ -34,26 +34,25 @@ def test_cleanup_new_image(cli, runtime, tmp_path):
     # workaround for https://github.com/ansible/ansible-runner/issues/758
     os.mkdir(str(tmp_path / 'project'))
 
+    # force no colors so that we can JSON load ad hoc output
+    os.mkdir(str(tmp_path / 'env'))
+    with open(str(tmp_path / 'env' / 'envvars'), 'w') as f:
+        f.write('{"ANSIBLE_NOCOLOR": "true"}')
+
     # assure that the image is usable in ansible-runner as an EE
     r = cli([
-        'run', str(tmp_path), '-m', 'slurp', '-a', 'src=/tmp/for_test.txt', '--hosts=localhost',
+        'run', str(tmp_path), '-m', 'slurp', '-a', 'src=/tmp/for_test.txt', '--hosts=localhost', '--ident', 'for_test',
         '--container-image', image_name, '--process-isolation', '--process-isolation-executable', runtime
     ])
-    print('before parse')
-    print(r.stdout)
-    print('to load')
-    print(r.stdout[r.stdout.index('{'):r.stdout.index('}') + 1])
-    # TODO: can't rely on r.stdout due to ansii encoding, probably have to dive into job events stdout
-    data = json.loads(r.stdout[r.stdout.index('{'):r.stdout.index('}') + 1])
-    print('data')
-    print(data)
+    stdout = r.stdout
+    data = json.loads(stdout[stdout.index('{'):stdout.index('}') + 1])
     assert 'content' in data
-    assert special_string == b64decode(data['content']).strip()
+    assert special_string == str(b64decode(data['content']).strip(), encoding='utf-8')
 
-    image_ct = cleanup_images(images=[image_name])
+    image_ct = cleanup_images(images=[image_name], runtime=runtime)
     assert image_ct == 1
     prune_images()  # May or may not do anything, depends on docker / podman
 
     assert layer_id not in cli([runtime, 'images'], bare=True).stdout  # establishes that cleanup was genuine
 
-    assert cleanup_images(images=[image_name]) == 0  # should be no-op
+    assert cleanup_images(images=[image_name], runtime=runtime) == 0  # should be no-op

3aabbd60dd9f4b1a2841e6176e667a098b1dcaf1
Sam Doran <sdoran@redhat.com>
2021-10-05 10:51:29 -0400

    Ensure the mode of the UUID file is set properly
    Add parameters for accepting mode.
    Add tests for ensuring the mode is set properly.

--- ansible_runner/utils/capacity.py
+++ ansible_runner/utils/capacity.py
@@ -24,7 +24,7 @@ def get_mem_in_bytes():
         return error
 
 
-def ensure_uuid(uuid_file_path=None, mode=0o600):
+def ensure_uuid(uuid_file_path=None, mode=0o0600):
     if uuid_file_path is None:
         uuid_file_path = Path.home().joinpath('.ansible_runner_uuid')
 
@@ -34,15 +34,22 @@ def ensure_uuid(uuid_file_path=None, mode=0o600):
         return saved_uuid.strip()
     else:
         # Generate a new UUID if file is not found
-        newly_generated_uuid = _set_uuid()
+        newly_generated_uuid = _set_uuid(mode)
         return newly_generated_uuid
 
 
-def _set_uuid(uuid_file_path=None, mode=0o600):
+def _set_uuid(uuid_file_path=None, mode=0o0600):
     if uuid_file_path is None:
         uuid_file_path = Path.home().joinpath('.ansible_runner_uuid')
 
     generated_uuid = str(uuid.uuid4())
+
+    if not uuid_file_path.exists():
+        # Ensure the file starts with correct permissions
+        uuid_file_path.touch(mode)
+
+    # Ensure the correct permissions if the file exists
+    uuid_file_path.chmod(mode)
 
     # Store the newly-generated UUID in a new file in home dir
     uuid_file_path.write_text(generated_uuid)
--- test/unit/utils/capacity/test_uuid.py
+++ test/unit/utils/capacity/test_uuid.py
@@ -14,12 +14,50 @@ def mock_uuid(mocker):
     return uuid
 
 
-def test_set_uuid(mock_uuid, tmp_path):
+@pytest.fixture
+def mock_home_path(mocker, tmp_path):
+    mocker.patch('ansible_runner.utils.capacity.Path.home', return_value=tmp_path)
+
+
+def test_set_uuid(mock_uuid, mock_home_path, tmp_path):
+    uuid = _set_uuid()
+
+    expected_path = tmp_path / '.ansible_runner_uuid'
+    assert uuid == mock_uuid
+    assert expected_path.exists()
+    assert expected_path.stat().st_mode == 0o100600
+    assert expected_path.read_text() == mock_uuid
+
+
+def test_set_uuid_mode(mock_uuid, tmp_path, mock_home_path):
+    uuid = _set_uuid(mode=0o750)
+
+    expected_path = tmp_path / '.ansible_runner_uuid'
+    assert uuid == mock_uuid
+    assert expected_path.exists()
+    assert expected_path.stat().st_mode == 0o100750
+    assert expected_path.read_text() == mock_uuid
+
+
+def test_set_uuid_change_mode(mock_uuid, tmp_path):
+    uuid_path = tmp_path / 'uuid'
+    uuid_path.touch(0o777)
+
+    uuid = _set_uuid(uuid_path)
+
+    assert uuid == mock_uuid
+    assert uuid_path.exists()
+    assert uuid_path.stat().st_mode == 0o100600
+    assert uuid_path.read_text() == mock_uuid
+
+
+def test_set_uuid_path(mock_uuid, tmp_path):
     uuid_path = tmp_path / 'uuid'
     uuid = _set_uuid(uuid_path)
 
     assert uuid == mock_uuid
     assert uuid_path.exists()
+    assert uuid_path.stat().st_mode == 0o100600
     assert uuid_path.read_text() == mock_uuid
 
 
@@ -29,6 +67,15 @@ def test_set_uuid_bad_path(mock_uuid, tmp_path):
         _set_uuid(uuid_path)
 
 
+def test_ensure_uuid(mocker, mock_uuid, mock_home_path, tmp_path):
+    mock_set_uuid = mocker.patch('ansible_runner.utils.capacity._set_uuid', return_value=mock_uuid)
+
+    uuid = ensure_uuid()
+
+    assert uuid == mock_uuid
+    mock_set_uuid.assert_called_with(0o600)
+
+
 def test_ensure_uuid_does_not_exist(mocker, mock_uuid, tmp_path):
     mock_set_uuid = mocker.patch('ansible_runner.utils.capacity._set_uuid', return_value=mock_uuid)
 
@@ -36,7 +83,7 @@ def test_ensure_uuid_does_not_exist(mocker, mock_uuid, tmp_path):
     uuid = ensure_uuid(uuid_path)
 
     assert uuid == mock_uuid
-    assert mock_set_uuid.call_count == 1
+    mock_set_uuid.assert_called_once_with(0o600)
 
 
 def test_ensure_uuid_exists(mocker, mock_uuid, tmp_path):

dc74d468e71d56c2eb328cfea01d86c4f1725809
Jim Ladd <jladd@redhat.com>
2021-10-06 13:22:33 -0700

    add test_cli_kill_cleanup
    Co-authored-by: Alan Rominger <arominge@redhat.com>

--- test/integration/containerized/test_cli_containerized.py
+++ test/integration/containerized/test_cli_containerized.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
+import signal
+import time
+
+from uuid import uuid4
 
 import pytest
 
@@ -29,3 +33,38 @@ def test_playbook_run(cli, skip_if_no_podman):
 def test_provide_env_var(cli, skip_if_no_podman, test_data_dir):
     r = cli(['run', os.path.join(test_data_dir, 'job_env'), '-p', 'printenv.yml'])
     assert 'gifmyvqok2' in r.stdout, r.stdout
+
+@pytest.mark.serial
+def test_cli_kill_cleanup(cli, skip_if_no_podman, test_data_dir):
+    unique_string = str(uuid4()).replace('-', '')
+    ident = f'kill_test_{unique_string}'
+    pdd = os.path.join(test_data_dir, 'sleep')
+    cli_args = ['start', pdd, '-p', 'sleep.yml', '--ident', ident, '--process-isolation', '--process-isolation-executable', 'podman']
+    cli(cli_args)
+
+    def container_is_running():
+        r = cli(['podman', 'ps', '-f', f'name=ansible_runner_{ident}', '--format={{.Names}}'], bare=True)
+        return ident in r.stdout
+
+    tries = 5
+    for i in range(tries):
+        if container_is_running():
+            break
+        time.sleep(1)
+    else:
+        assert container_is_running()
+
+    # give playbook execution time to start
+    time.sleep(5)
+
+    # Here, we will do sigterm to kill the parent process, it should handle this gracefully
+    with open(os.path.join(pdd, 'pid'), 'r') as f:
+        pid = int(f.read().strip())
+    os.kill(pid, signal.SIGTERM)
+
+    for i in range(tries):
+        if not container_is_running():
+            break  # yay, test passed
+        time.sleep(1)
+    else:
+        assert not container_is_running()

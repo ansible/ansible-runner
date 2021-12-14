ce45e49ffa58d97665919ee2bdd37c546d456c4b
Sam Doran <sdoran@redhat.com>
2021-12-14 13:11:08 -0500

    [release_2.0] Write auto-created private data dirs to pytest dir instead of /tmp (#916) (#942)
    [release_2.0] Write auto-created private data dirs to pytest dir instead of /tmp (#916)
    
    Backport of #916 for Ansible Runner 2.0.
    
    Reviewed-by: David Shrewsbury <None>
    Reviewed-by: None <None>

--- ansible_runner/defaults.py
+++ ansible_runner/defaults.py
@@ -1,2 +1,7 @@
 default_process_isolation_executable = 'podman'
 default_container_image = 'quay.io/ansible/ansible-runner:devel'
+
+# values passed to tempfile.mkdtemp to generate a private data dir
+# when user did not provide one
+AUTO_CREATE_NAMING = '.ansible-runner-'
+AUTO_CREATE_DIR = None

ecae0611a08b2f99a9cb80d20667358f3392ec81
David Shrewsbury <Shrews@users.noreply.github.com>
2022-01-20 15:26:23 -0500

    Create GitHub Actions workflow for running tests (#944) (#962)
    [backport] [release_2.1] Create GitHub Actions workflow for running tests (#944)
    
    Add GHA CI workflow. Includes sanity, unit, and integration tests.
    Backport of #944
    Co-authored-by: David Shrewsbury dshrewsb@redhat.com
    (cherry picked from commit 16cc1fe)
    
    Reviewed-by: Alexander Sowitzki <dev@eqrx.net>
    Reviewed-by: None <None>

--- test/integration/test_runner.py
+++ test/integration/test_runner.py
@@ -12,6 +12,7 @@ from ansible_runner import Runner
 from ansible_runner.exceptions import AnsibleRunnerException
 
 
+@pytest.mark.xfail(reason='Test is unstable')
 def test_password_prompt(rc):
     rc.command = [sys.executable, '-c' 'import time; print(input("Password: "))']
     rc.expect_passwords[re.compile(r'Password:\s*?$', re.M)] = '1234'

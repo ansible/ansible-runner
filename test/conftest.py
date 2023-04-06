b0ee62728bf2782f4d0f9e67464514a06a5d6af8
Abhijeet Kasurde <akasurde@redhat.com>
2023-04-06 12:56:11 -0700

    Remove pkg_resources use (#1224)
    * Remove pkg_resources use
    * pkg_resources is deprecated in favor of importlib.metadata
    * expand < 3.10 importlib-metadata polyfill version constraints
    
    Fixes: #1223
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>
    Co-authored-by: Matt Davis <6775756+nitzmahone@users.noreply.github.com>
    (cherry picked from commit 3d6f66bfcc6d12bb497c896765940afbb20f6227)

--- test/conftest.py
+++ test/conftest.py
@@ -2,11 +2,10 @@ import shutil
 
 from pathlib import Path
 from packaging.version import Version
-import subprocess
 
 from ansible_runner import defaults
+from ansible_runner.utils.importlib_compat import importlib_metadata
 
-import pkg_resources
 import pytest
 
 
@@ -35,9 +34,9 @@ def is_pre_ansible211():
     """
 
     try:
-        if pkg_resources.get_distribution('ansible-core').version:
+        if importlib_metadata.version("ansible-core"):
             return False
-    except pkg_resources.DistributionNotFound:
+    except importlib_metadata.PackageNotFoundError:
         # Must be ansible-base or ansible
         return True
 
@@ -51,18 +50,10 @@ def skipif_pre_ansible211(is_pre_ansible211):
 @pytest.fixture(scope="session")
 def is_pre_ansible212():
     try:
-        base_version = (
-            subprocess.run(
-                "python -c 'import ansible; print(ansible.__version__)'",
-                capture_output=True,
-                shell=True,
-            )
-            .stdout.strip()
-            .decode()
-        )
+        base_version = importlib_metadata.version("ansible")
         if Version(base_version) < Version("2.12"):
             return True
-    except pkg_resources.DistributionNotFound:
+    except importlib_metadata.PackageNotFoundError:
         pass
 
 

a227ae81b6e85e2e30108b9e6c4a453562540d1e
Matt Martz <matt@sivel.net>
2023-04-13 17:53:46 -0500

    Drop pbr in favor of setuptools_scm

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import pathlib
+
 from setuptools import setup
 
+here = pathlib.Path(__file__).parent.resolve()
+
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    install_requires=install_requires,
 )

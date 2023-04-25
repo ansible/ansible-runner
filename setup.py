783a5067b9975438843806900d63059ad9878a8f
Matt Martz <matt@sivel.net>
2023-04-25 13:11:52 -0500

    move install deps into setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-import pathlib
-
 from setuptools import setup
 
-here = pathlib.Path(__file__).parent.resolve()
-
-install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
-
-setup(
-    install_requires=install_requires,
-)
+setup()

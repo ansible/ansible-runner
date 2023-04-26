e2e89b0cac13e83f08d0a76a7d31939c742deff6
Matt Martz <matt@sivel.net>
2023-04-26 17:56:33 -0500

    Drop pbr in favor of setuptools_scm (#1234)

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     "sphinx_ansible_theme",
-    'pbr.sphinxext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -175,7 +174,7 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Options for todo extension ----------------------------------------------
 

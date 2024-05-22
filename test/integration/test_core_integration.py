abfff721a2a12e07f57d7d9e1d63e33ebf42689e
David Shrewsbury <Shrews@users.noreply.github.com>
2024-05-22 11:34:04 -0400

    Update core integration testing versions (#1360)
    (cherry picked from commit 42ef262ddd064df3d7fead814793d7c93ceff442)

--- test/integration/test_core_integration.py
+++ test/integration/test_core_integration.py
@@ -7,8 +7,8 @@ from ansible_runner.interface import run
 TEST_BRANCHES = (
     'devel',
     'milestone',
-    'stable-2.16',   # current stable
-    'stable-2.15',   # stable - 1
+    'stable-2.17',   # current stable
+    'stable-2.16',   # stable - 1
 )
 
 

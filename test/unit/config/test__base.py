8b3f06a6900acebbb7a019159d4eafd871af6439
Max Mitschke <mmitschk@redhat.com>
2025-03-19 21:59:08 -0400

    Fixing test_containerization_settings for test__base

--- test/unit/config/test__base.py
+++ test/unit/config/test__base.py
@@ -318,7 +318,7 @@ def test_containerization_settings(tmp_path, runtime, mocker):
         '--workdir',
         '/runner/project',
         '-v', f'{str(tmp_path)}/.ssh/:/home/runner/.ssh/',
-        '-v', f'{str(tmp_path)}/.ssh/:/root/.ssh/',
+        '-v', f'{str(tmp_path)}/.ssh/:/root/.ssh/'
     ]
 
     if os.path.exists('/etc/ssh/ssh_known_hosts'):
@@ -330,6 +330,8 @@ def test_containerization_settings(tmp_path, runtime, mocker):
     expected_command_start.extend([
         '-v', f'{rc.private_data_dir}/artifacts/:/runner/artifacts/:Z',
         '-v', f'{rc.private_data_dir}/:/runner/:Z',
+        '-v', '/host1:/container1',
+        '-v', 'host2:/container2',
         '--env-file', f'{rc.artifact_dir}/env.list',
     ])
 
